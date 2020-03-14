acb72549c2cf21b93f8e77011bbb3cce15b5cabb
BeomSeok <nbs0105@gmail.com>
2020-03-14 16:45:12 +0900

    Can you add the missing a space between = and new?
    space 수정

--- savedate.js
+++ savedate.js
@@ -1,8 +1,4 @@
 var fs = require('fs');
-<<<<<<< HEAD
-var date =new Date();
-=======
 var date = new Date();
->>>>>>> master
 fs.writeFileSync('latestdate.txt', date, 'utf8');
   console.log("Sync Write Complete");
