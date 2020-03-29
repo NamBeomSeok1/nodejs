bad9b07e3f88ef1b754f76c48d910a7afce69ead
BeomSeok <nbs0105@gmail.com>
2020-03-29 20:05:18 +0900

    Update
    오류 수정
    const, file format, on.end

--- Practice6/notes_server.js
+++ Practice6/notes_server.js
@@ -1,29 +1,30 @@
 const http = require('http');
 const memos = [];
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type' : 'text/plain' });
-    if(req.method==='POST'){
-        if(req.url==='/add'){
-            req.on('data',(data)=>
-            {
-                data = data.toString();
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    if (req.method === 'POST') {
+        if (req.url === '/add') {
+            const chunks = [];
+            req.on('data', (data) => {
                 console.log(data)
-                memos.push(data);
+                chunks.push(data);
+            }).on('end',()=>{
+                const fullBody = Buffer.concat(chunks).toString();
+                memos.push(fullBody); 
             })
         }
     }
-    
-    else if(req.method==="GET"){
-        if(req.url==="/list"){
-            res.writeHead(200, { 'Content-Type' : 'text/plain' });
-                    let string = JSON.stringify(memos);
-                    res.write(string);
-                    res.end();
-                
+
+    else if (req.method === "GET") {
+        if (req.url === "/list") {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            const string = JSON.stringify(memos);
+            res.write(string);
+            res.end();
+
+        }
     }
-  }
 });
+        
 
 server.listen(80);
-  
-
